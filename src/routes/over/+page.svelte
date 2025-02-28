36981129d0b4b21849c8bf90cdf9efab5f7106d0
Geert-Jan Bogaerts <gj@raker.nl>
2025-02-28 14:43:14 +0100

    Over Raker pagina

---
+++ src/routes/over/+page.svelte
@@ -0,0 +1,67 @@
+<article>
+	<h1>Over Raker</h1>
+	<p>
+		<img
+			src="https://raker.nl/wp-content/uploads/2025/01/GJ-1-scaled.jpg"
+			alt="Portret van Geert-Jan Bogaerts"
+		/>Mijn naam is Geert-Jan Bogaerts. Ik ben journalist van roeping, maar niet meer van beroep. Ik
+		werk nu als hoofd Digitaal bij de VPRO, maar ik begon mijn professionele carriere als journalist
+		bij de Nijmeegse Stadskrant, in 1988. In 1990 werd ik economieredacteur bij de Volkskrant, en in
+		1997 correspondent EU en NAVO in Brussel. Na nog eens zes jaar, in 2003, keerde ik terug naar
+		Amsterdam en ben ik veel meer de online kant opgegaan.
+	</p>
+	<p>
+		Ik was al vanaf mijn allereerste kennismaking, ik denk rond 1993, gefascineerd door het
+		internet. Ik vond het technisch interessant en zag hoe potentieel impactvol het was in zo'n
+		beetje alle domeinen van onze samenleving. In de jaren daarna zag ik het internet groeien, en
+		zag ik ook hoe ons ambacht, dat van de journalist, aan het veranderen ging. Dat was de
+		voornaamste reden om chef internet en later Hoofd Online bij de Volkskrant te worden.
+	</p>
+	<p>
+		Dat heb ik tot 2010 gedaan. Ik vertrok toen de Belgen kwamen - toen DPG Media de Volkskrant en
+		andere kranten overnam en het online beleid over een heel andere boeg wilde gooien.
+		Centralisatie was het idee, alle operatie vanuit Brussel. Daar was ik fel op tegen. En omdat ik
+		langzamerhand toch het gevoel had, na 20 jaar Volkskrant, dat het tijd was voor wat anders, ben
+		ik onder de naam Raker in 2010 gaan freelancen.
+	</p>
+	<p>
+		Ik heb in die periode onder andere adviezen geschreven over online strategie, ik heb
+		gepubliceerd, ik heb trainingen en lessen gegeven over online schrijven en werken, en ik was
+		docent op de Masteropleiding Journalistiek aan de RUG.
+	</p>
+	<p>
+		In 2014 werd ik benaderd voor mijn huidige baan bij de VPRO en is mijn blog praktisch meteen op
+		pauze gegaan. En ook het bedrijf Raker is gestopt. Die blogpauze heeft ruim tien jaar geduurd,
+		maar in januari 2025 besloot ik de draad weer op te pakken. Ik krijg steeds meer behoefte aan
+		een vaste plek waar ik mijn gedachten kan formuleren. Eigenlijk interesseert het me nog niet
+		eens zoveel hoeveel mensen dit lezen; ik schrijf primair voor mezelf.
+	</p>
+	<p>
+		Ik heb politicologie gestudeerd in Nijmegen. Ik heb lange perioden (7 maanden tot zes jaar)
+		gewoond en gewerkt in de VS, in Midden-Amerika en in België.  Mijn LinkedIn profiel vind je
+		hier.
+	</p>
+	<h2>Raker</h2>
+	<p>
+		Raker is de naam van het freelance bedrijfje waarmee ik ooit, in 1988, de journalistiek inging.
+		Ik ben de naam altijd blijven hanteren als een soort reminder of the past, van waar ik vandaan
+		kom. Mensen hebben allerlei connotaties bij de naam Raker, en de achtergrond is, eerlijk gezegd,
+		een beetje romantisch. Mijn grote voorbeelden in de beginfase van mijn journalistieke loopbaan
+		waren de grote Amerikaanse muckrakers, de journalisten en schrijvers die tegen de klippen op
+		misstanden in de maatschappij aan het licht brachten. Ida Tarbell, Ray Stannard Baker, en veel
+		later natuurlijk Bob Woodward en Carl Bernstein. Van muckraker werd de naam Raker afgeleid; en
+		de rest is geschiedenis.
+	</p>
+</article>
+
+<style>
+	p {
+		margin: 1rem 0;
+	}
+
+	img {
+		float: left;
+		max-width: 30rem;
+		padding-right: 1rem;
+	}
+</style>
